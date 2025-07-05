3929cd32293a5cbfb35932b4aea63d6ef0df93ff
vbello_tech <vbellotech@gmail.com>
2025-07-05 04:04:50 +0100

    feat: add dashboard frontend

--- store/models.py
+++ store/models.py
@@ -12,3 +12,23 @@ class ProductCategory(models.Model):
 
     def __str__(self):
         return self.category
+#
+#
+# category_list = ProductCategory.objects.all().values_list('category', 'category')
+# product_cat_choice = []
+# for item in category_list:
+#     product_cat_choice.append(item)
+#
+#
+# class Product(models.Model):
+#     name = models.CharField(max_length=300)
+#     color = models.CharField(max_length=300)
+#     type = models.CharField(max_length=300, choices=product_cat_choice)
+#     serial_no = models.IntegerField()
+#     spec = models.CharField(max_length=300)
+#     quantity = models.IntegerField()
+#     checked_in = models.BooleanField(default=False)
+#     sold_out = models.BooleanField(default=False)
+#
+#     def __str__(self):
+#         return self.name
